5aa08c8750a042415f29ed2d9ee7d040ce52a92c
shali dissanayaka <dissshalimmw25@gmail.com>
2025-01-03 23:06:49 +0530

    remove whatsapp attribute

--- models/user.js
+++ models/user.js
@@ -31,12 +31,7 @@ const userSchema = new mongoose.Schema({
     phone :{
         type :String,
         required : true   
-    },
-    whatsApp :{
-        type :String,
-        required : true   
     }
-
 });
 
 const User = mongoose.model("User",userSchema);
